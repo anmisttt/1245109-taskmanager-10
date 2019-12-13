dfcd4740e5d6c233734e3c2260e14f49867aa079
anmisttt <111nnn75@gmail.com>
2019-12-13 16:51:31 +0300

    module 4

--- src/components/task.js
+++ src/components/task.js
@@ -1,5 +1,5 @@
 import {MONTH_NAMES} from '../const.js';
-import {formatTime} from '../utils.js';
+import {createElement, formatTime} from '../utils.js';
 
 const createHashtagsMarkup = (hashtags) => {
   return hashtags
@@ -15,7 +15,7 @@ const createHashtagsMarkup = (hashtags) => {
     .join(`\n`);
 };
 
-export const createTaskTemplate = (task) => {
+const createTaskTemplate = (task) => {
   const {
     description,
     tags,
@@ -83,3 +83,27 @@ export const createTaskTemplate = (task) => {
 
                 < /article>`);
 };
+
+export default class Task {
+  constructor(task) {
+    this._task = task;
+
+    this._element = null;
+  }
+
+  getTemplate() {
+    return createTaskTemplate(this._task);
+  }
+
+  getElement() {
+    if (!this._element) {
+      this._element = createElement(this.getTemplate());
+    }
+
+    return this._element;
+  }
+
+  removeElement() {
+    this._element = null;
+  }
+}
